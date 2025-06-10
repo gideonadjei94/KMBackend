35179d2838e3abdddbb30aa7e884b8a49fa0c826
gideonadjei <gideonadjei94@gmail.com>
2025-06-10 11:49:47 +0000

    Initial Commit

---
+++ src/main/java/com/gideon/knowmate/KnowmateApplication.java
@@ -0,0 +1,13 @@
+package com.gideon.knowmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gideon/knowmate/KnowmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gideon.knowmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
