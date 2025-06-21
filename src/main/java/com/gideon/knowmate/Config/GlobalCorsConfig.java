928899a67fbbc78d975555c2cb8026c21803332e
gideonadjei <gideonadjei94@gmail.com>
2025-06-21 01:34:35 +0000

    CORS issue fix

--- src/main/java/com/gideon/knowmate/Config/GlobalCorsConfig.java
+++ src/main/java/com/gideon/knowmate/Config/GlobalCorsConfig.java
@@ -14,7 +14,7 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                  registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/", "https://knowmate.onrender.com")
+                        .allowedOrigins("http://localhost:5173", "https://knowmate.onrender.com")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowCredentials(true);
