bc9a0f23d11f163ab9a4df24d3a392f0d39d35a2
gideonadjei <gideonadjei94@gmail.com>
2025-06-28 21:00:05 +0000

    Cors Issue

--- src/main/java/com/gideon/knowmate/Entity/FlashCard.java
+++ src/main/java/com/gideon/knowmate/Entity/FlashCard.java
@@ -1,9 +1,15 @@
 package com.gideon.knowmate.Entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FlashCard {
     private String term;
     private String definition;
